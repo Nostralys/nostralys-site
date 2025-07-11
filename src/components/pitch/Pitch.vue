1bcde0172e1af49b4ee31f016a07143966be22d8
alexis-moins <sinus.nacre0v@icloud.com>
2025-07-11 17:00:52 +0200

    feat!: add pitch section

---
+++ src/components/pitch/Pitch.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import PitchSlide from './PitchSlide.vue';
+
+import gridGeneration from '../../images/gifs/grid-generation.gif'
+
+const sections = [
+	{
+		text: 'Génération procédurale',
+		image: gridGeneration
+	},
+]
+</script>
+
+<template>
+	<section class="w-full h-3/5 md:h-full flex flex-col md:flex-row justify-center items-center gap-4 bg-[#fffaf4]">
+		<PitchSlide :sections="sections" />
+	</section>
+</template>
